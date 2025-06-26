ea5348968380cdfe912a00eaa0077a0bccddad16
imeuro <mauro.fioravanzi@gmail.com>
2025-06-26 17:28:02 +0200

    mrc25 theme - footer

--- wp-content/themes/mrc25/footer.php
+++ wp-content/themes/mrc25/footer.php
@@ -12,10 +12,20 @@
                     ));
                     ?>
                 </div>
+                <div class="footer-societa">
+                    <p>
+                        <strong>MILANO RE COSTRUZIONI S.R.L.</strong><br>
+                        Sede legale: VIA FONTANA 17 - 20122 - MILANO (MI)<br>
+                        P.IVA/CF: 13800780960<br>
+                        Rea: 2744518
+                    </p>
+                </div>
             </div>
             <div class="site-info">
                 <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. 
-                <?php esc_html_e('Tutti i diritti riservati.', 'mrc25'); ?></p>
+                <?php esc_html_e('Tutti i diritti riservati.', 'mrc25'); ?>
+                - <a href="<?php echo home_url('/privacy-policy'); ?>">Privacy Policy</a> - <a href="<?php echo home_url('/cookie-policy'); ?>">Cookie Policy</a>
+            </p>
             </div><!-- .site-info -->
         </div><!-- .container -->
     </footer><!-- #colophon -->
